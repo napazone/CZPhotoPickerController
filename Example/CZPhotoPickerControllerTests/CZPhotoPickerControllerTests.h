9a65addad01e6ed72ef0d61fc77c2b4b6457b5e0
Seivan Heidari <seivan@kth.se>
2013-08-21 21:29:24 +0200

    Delete old project.

---
+++ Example/CZPhotoPickerControllerTests/CZPhotoPickerControllerTests.h
@@ -0,0 +1,13 @@
+//
+//  CZPhotoPickerControllerTests.h
+//  CZPhotoPickerControllerTests
+//
+//  Created by Seivan Heidari on 8/21/13.
+//  Copyright (c) 2013 Care Zone, Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CZPhotoPickerControllerTests : SenTestCase
+
+@end
---
+++ Example/CZPhotoPickerControllerTests/CZPhotoPickerControllerTests.m
@@ -0,0 +1,32 @@
+//
+//  CZPhotoPickerControllerTests.m
+//  CZPhotoPickerControllerTests
+//
+//  Created by Seivan Heidari on 8/21/13.
+//  Copyright (c) 2013 Care Zone, Inc. All rights reserved.
+//
+
+#import "CZPhotoPickerControllerTests.h"
+
+@implementation CZPhotoPickerControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CZPhotoPickerControllerTests");
+}
+
+@end
