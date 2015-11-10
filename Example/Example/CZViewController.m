8b62d758d622959095373f830363932968a532fe
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2015-11-10 12:04:36 -0800

    Add the lib to example app as a framework

--- Example/Example/CZViewController.m
+++ Example/Example/CZViewController.m
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+@import CZPhotoPickerController;
 #import <QuartzCore/QuartzCore.h>
-#import "CZPhotoPickerController.h"
 #import "CZViewController.h"
 
 @interface CZViewController ()
