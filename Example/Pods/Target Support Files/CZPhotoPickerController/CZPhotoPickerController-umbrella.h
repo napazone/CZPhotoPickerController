0a6d53ad0c56f61136c880d51a1c2fc04fe7f2b7
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2015-11-10 12:06:42 -0800

    Rerun `pod install`

---
+++ Example/Pods/Target Support Files/CZPhotoPickerController/CZPhotoPickerController-umbrella.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "CZCroppingScrollView.h"
+#import "CZCropPreviewOverlayView.h"
+#import "CZPhotoPickerController.h"
+#import "CZPhotoPreviewViewController.h"
+
+FOUNDATION_EXPORT double CZPhotoPickerControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char CZPhotoPickerControllerVersionString[];
+
